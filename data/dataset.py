aded936b26b6cff97af598f3070f10c543b86ebe
Sofiia Ilnytska <112697394+Sofiia-Mit2i@users.noreply.github.com>
2025-03-31 14:21:11 +0200

    Update dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -2,21 +2,19 @@ import torch
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from medmnist import BloodMNIST
-
-def get_dataloaders(BATCH_SIZE=32, num_workers=2):
 """
 Creates and returns DataLoaders for the BloodMNIST dataset.
 - batch_size (int): Number of samples per training batch. Default: 32
 - num_workers (int): Number of subprocesses for data loading. Default: 2
 """
-# preprocessing
+def get_dataloaders(BATCH_SIZE=32, num_workers=2):
         transform = transforms.Compose([
             # Convert PIL Image/numpy.ndarray to torch.Tensor and scale to [0, 1]
             transforms.ToTensor(),  
             transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])  
-        # Normalize tensor values to range [-1, 1] using:
-        # input[channel] = (input[channel] - mean[channel]) / std[channel]
-        # For RGB images 3-channel normalization:
+                # Normalize tensor values to range [-1, 1] using:
+                # input[channel] = (input[channel] - mean[channel]) / std[channel]
+                # For RGB images 3-channel normalization:
         ])
         
         # --- Dataset Loading ---
