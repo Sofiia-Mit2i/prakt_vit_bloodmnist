b01bbf5806611eca52be8c7b7b993d89f3bb1b7b
Ilnytska, Sofiia <sofiia.ilnytska@bih-charite.de>
2025-04-02 13:42:19 +0200

    sofiia files

--- 3D_ViT/data/dataset.py
+++ 3D_ViT/data/dataset.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import torch
 import nibabel as nib
 from torch.utils.data import DataLoader
@@ -81,62 +81,3 @@ def get_dataloaders(batch_size=16, num_workers=4, prefetch_factor=2, pin_memory=
     print(f"Sample labels: {labels[:5]}")  # First 5 labels
 
     return train_loader, train_loader_at_eval, val_loader, test_loader
-=======
-import torch
-from torch.utils.data import DataLoader
-from torchvision import transforms
-from medmnist import FractureMNIST3D
-import nibabel as nib
-
-"""
-Creates and returns DataLoaders for the BloodMNIST dataset.
-- batch_size (int): Number of samples per training batch. Default: 32
-- num_workers (int): Number of subprocesses for data loading. Default: 2
-"""
-def get_dataloaders(batch_size=16, num_workers=4):
-        transform = transforms.Compose([
-            transforms.ToTensor(),  
-            transforms.Normalize(mean=[0.5], std=[0.5])  
-        ])
-        
-        # --- Dataset Loading ---
-        train_dataset = FractureMNIST3D(
-        split='train', 
-        download=True, 
-        transform=transform,
-#npz image
-#
-        target_transform=lambda x: torch.tensor(x).squeeze().long()
-    )
-        test_dataset = FractureMNIST3D(
-        split='test', 
-        download=True, 
-        transform=transform,
-        target_transform=lambda x: torch.tensor(x).squeeze().long()
-    )
-        
-        
-        
-        # --- DataLoader Configuration ---
-        train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
-        train_loader_at_eval = DataLoader(dataset=train_dataset, batch_size=2*batch_size, shuffle=False, num_workers=num_workers)
-        test_loader = DataLoader(dataset=test_dataset, batch_size=2*batch_size, shuffle=False, num_workers=num_workers)
-
-        # Add this to your dataset loading code
-        sample = next(iter(train_loader))
-        print("Input shape:", sample[0].shape)  # Should be [B, C, D, H, W]
-        print("Target shape:", sample[1].shape)  # Should be [B]
-        
-        print(train_dataset)
-        print("===================")
-        print(test_dataset)
-        print("===================")
-        print(train_loader)
-
-        train_dataset.montage(length=1)
-        train_dataset.montage(length=10)
-
-        return train_loader, train_loader_at_eval, test_loader
-
-print(get_dataloaders())
->>>>>>> 295eab9 (erste Anpassungen für 3D)
