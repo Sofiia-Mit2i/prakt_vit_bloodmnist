d68dbf680cef1b8813de9413920971519375ee1d
Sofiia Ilnytska <112697394+Sofiia-Mit2i@users.noreply.github.com>
2025-04-02 11:53:08 +0200

    Update main_3D.py

--- 3D_ViT/main_3D.py
+++ 3D_ViT/main_3D.py
@@ -1,6 +1,7 @@
 import torch
 from data.dataset import get_dataloaders
 from encoder.input_embedding import InputEmbedding
+from enocder.encoder_block import EncoderBlock
 
 def main():
     # Set the batch size
