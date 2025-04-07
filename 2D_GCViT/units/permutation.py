da415cbdc6ca153bc59bb3c3330bfc00dc02ede7
Wiebke Teetz <wiebke.teetz@fu-berlin.de>
2025-04-07 13:38:11 +0200

    unit files

---
+++ 2D_GCViT/units/permutation.py
@@ -0,0 +1,20 @@
+def _to_channel_last(x):
+    """
+    Args:
+        x: (B, C, H, W)
+
+    Returns:
+        x: (B, H, W, C)
+    """
+    return x.permute(0, 2, 3, 1)
+
+
+def _to_channel_first(x):
+    """
+    Args:
+        x: (B, H, W, C)
+
+    Returns:
+        x: (B, C, H, W)
+    """
+    return x.permute(0, 3, 1, 2)
